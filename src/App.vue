174ef92e8c29ad029fef936f222c8be86e97d002
Anton Astremskyi <lukcytony@Antons-MBP.home>
2019-12-17 19:08:14 -0500

    posts limit added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>{{postsCount}}</h1>
     <div class="post" v-for="post in allPosts" :key="post.id">
       <h2>{{post.title}}</h2>
       <p>{{post.body}}</p>
@@ -8,19 +9,13 @@
 </template>
 
 <script>
-
-
+import { mapGetters, mapActions } from 'vuex';
 export default {
   name: "app",
-  computed: {
-    allPosts() {
-      return this.$store.getters.allPosts;
-    }
-  },
+  computed: mapGetters(["allPosts", "postsCount"]),
+  methods: mapActions(["fetchPosts"]),
    async mounted() {
-   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3');
-   const posts = await res.json();
-   this.posts = posts;
+     this.fetchPosts();
   }
 };
 </script>
